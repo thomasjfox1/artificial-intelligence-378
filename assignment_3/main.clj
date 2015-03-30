3ced523bee8a3572650608a91689fb9d94e920e2
Thomas J. Fox <thomasjfox1@gmail.com>
2015-03-29 20:55:13 -0500

    first attempt

--- assignment_3/main.clj
+++ assignment_3/main.clj
@@ -1,2 +1,16 @@
 ;Thomas J. Fox
 ;30 March 2015
+
+(defstruct errorCheck
+  :endlessLoop      ;Endless loop; the program never terminates
+  :derivedClass     ;Executes base class method when it should have executed derived class method.
+  :noOutput         ;Fails to produce any output; unclear whether it actually executed.
+  :loopShort        ;Loop executes too few times.
+  :loopLong         ;Loop executes too many times.
+  :illegalOp        ;Illegal operation attempted.
+  :inaccurateSome   ;Results are incorrect for a few cases.
+  :inaccurateAll    ;Results are incorrect for all cases.
+  :incomplete       ;Results are incomplete.
+  :dataDestroyed    ;A data item is being changed or destroyed with no obvious cause.
+                    ;. . any other reasonably common problems you think of . .
+  )
